2188634c27c382f173321b2ed1b41e175a5a004a
Nguyen Dat <vandat9xiloveyou@gmail.com>
2021-07-12 09:22:01 +0700

    Fixed media url

--- test_chat/urls.py
+++ test_chat/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/account/', include('account.urls')),
     path('api/chat/', include('chat.urls')),
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
 
 page_urls = [
